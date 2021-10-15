aae5110cea7fdd7dadc93e5a080ce6864522f3c4
Матвей Сугоняев <alienfoun@gmail.com>
2021-10-16 01:39:34 +0300

    16.10.2021. Fix

---
+++ sql.py
@@ -0,0 +1,28 @@
+import sqlite3 as sql
+def sql_insert(cur, tag_name, words_list):
+    cur.execute(f"INSERT INTO `Tags` VALUES ('{tag_name}', '{words_list}')")
+    con.commit()
+
+
+def sql_fetch(cur, tag_name):
+    cur.execute(f"select * from Tags where Tag='{tag_name}'")
+    rows = cur.fetchall()
+    return rows
+
+def sql_select_tags(tag, list_of_words):
+    if list_of_words != '':
+        sql_insert(cur, tag, list_of_words)
+        print('Данные успешно добавлены в базу данных')
+    else:
+        print('Данные не были добавлены в базу данных')
+
+
+con = sql.connect('Database.db') # подключение к бд
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS `Tags` (`Tag` STRING, `Words` STRING)")
+
+def sql_close():
+    con.commit()
+    cur.close()
+
