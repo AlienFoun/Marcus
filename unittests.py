ae17d913a670ab382ce17ca0938a2d88f33e8929
Матвей Сугоняев <alienfoun@gmail.com>
2021-10-17 20:23:38 +0300

    17.10.2021. add unitests

---
+++ unittests.py
@@ -0,0 +1,11 @@
+import unittest
+from helper import cutter
+class TestCutter(unittest.TestCase):
+    # начинается с test_
+    def test_cut(self):
+        self.assertEqual(cutter('я тебя люблю'.split()), ['я', 'тебя', 'люблю', 'я тебя', 'тебя люблю', 'я тебя люблю'])
+        self.assertEqual(cutter('I love you'.split()), ['I', 'love', 'you', 'I love', 'love you', 'I love you'])
+        self.assertEqual(cutter(1234567890), TypeError)
+
+if __name__ == '__main__':
+    unittest.main()
