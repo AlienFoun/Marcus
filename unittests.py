2da3a87477987bd50002c61791924fbf5ae241a1
Матвей Сугоняев <alienfoun@gmail.com>
2021-10-17 20:32:17 +0300

    17.10.2021. Fix

--- unittests.py
+++ unittests.py
@@ -5,7 +5,7 @@ class TestCutter(unittest.TestCase):
     def test_cut(self):
         self.assertEqual(cutter('я тебя люблю'.split()), ['я', 'тебя', 'люблю', 'я тебя', 'тебя люблю', 'я тебя люблю'])
         self.assertEqual(cutter('I love you'.split()), ['I', 'love', 'you', 'I love', 'love you', 'I love you'])
-        self.assertEqual(cutter(1234567890), TypeError)
+        self.assertEqual(cutter(1234567890), [])
 
 if __name__ == '__main__':
     unittest.main()
