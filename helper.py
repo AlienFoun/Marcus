32b786ffc79d1efafc71e4af4cdff337d0e29193
Матвей Сугоняев <alienfoun@gmail.com>
2021-10-18 16:25:10 +0300

    18.10.2021. Fix

--- helper.py
+++ helper.py
@@ -1,47 +1,48 @@
 import string
-from sql import *
 import json
+from sql import sql_fetch
 
-def cutter(text: list):
-    if type(text) == list and text != []:
+
+def cutter(text: list) -> list:
+    if text != [] and isinstance(text, list):
         total = []  # Создание списка для вывода
-        n = 0  # Счетчик количества символов
+        simbol = 0  # Счетчик количества символов
         while ' '.join(text) not in total:  # Проверка, находится ли входной текст в списке вывода
             prom = []  # создание/очистка промежуточного списка
             i = 0  # Счетчик цикла
             while i < len(text):
                 prom.append(text[i])  # Добавляем элементы в промежуточный список
-                if len(prom) == n + 1:  # Нужно нарезать список сначала по одному слову,
+                if len(prom) == simbol + 1:  # Нужно нарезать список сначала по одному слову,
                     # потом по 2, 3 и т.д., тут происходит проверка на количество символов
                     total.append(' '.join(prom))  # Если верно, то добавляем в список вывода
                     prom = []  # Очищаем промежуточный список
-                    i -= n  # Сдвигаемся на n индексов назад
+                    i -= simbol  # Сдвигаемся на n индексов назад
                 i += 1
-            n += 1
-        return total # Возвращаем список вывода
+            simbol += 1
+        return total  # Возвращаем список вывода
     else:
         return []
 
 
-def appends(cur, tag_name: str):
+def appends(cursor, tag_name: str) -> list:
     tag_list_name = []
-    rows = sql_fetch(cur, tag_name)
+    rows = sql_fetch(cursor, tag_name)
     for row in rows:
         words = json.loads(row[1])
-        tag_list_name+=words
+        tag_list_name += words
     return tag_list_name
 
 
-def words_list_gen(lists: list, tag_list_name: list):
+def words_list_gen(lists: list, tag_list_name: list) -> list:
     not_join_words_list = []
-    for i in range(len(lists)):
-        if lists[i] not in tag_list_name:
-            not_join_words_list.append(lists[i])
+    for element in lists:
+        if element not in tag_list_name:
+            not_join_words_list.append(element)
     return not_join_words_list
 
 
-def sanitizer(clear_text: str):
+def sanitizer(clear_text: str) -> str:
     punc = string.punctuation
-    for i in range(len(punc)):
-        clear_text = clear_text.replace(punc[i], '')
+    for element in punc:
+        clear_text = clear_text.replace(element, '')
     return clear_text
--- sql.py
+++ sql.py
@@ -1,31 +1,31 @@
 import sqlite3 as sql
 
 
-def sql_insert(cur, tag_name, words_list):
+def sql_insert(cur, tag_name: str, words_list: str) -> None:
     cur.execute(f"INSERT INTO `Tags` VALUES ('{tag_name}', '{words_list}')")
     con.commit()
 
 
-def sql_fetch(cur, tag_name):
+def sql_fetch(cur, tag_name: str) -> list:
     cur.execute(f"select * from Tags where Tag='{tag_name}'")
-    rows = cur.fetchall()
+    rows = cursor.fetchall()
     return rows
 
 
-def sql_select_tags(tag, list_of_words):
+def sql_select_tags(tag, list_of_words: str) -> None:
     if list_of_words != '[]':
-        sql_insert(cur, tag, list_of_words)
+        sql_insert(cursor, tag, list_of_words)
         print('Данные успешно добавлены в базу данных')
     else:
         print('Данные не были добавлены в базу данных')
 
 
-def sql_close():
+def sql_close() -> None:
     con.commit()
-    cur.close()
+    cursor.close()
 
 
 con = sql.connect('Database.db')  # подключение к бд
 with con:
-    cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS `Tags` (`Tag` STRING, `Words` STRING)")
+    cursor = con.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS `Tags` (`Tag` STRING, `Words` STRING)")
