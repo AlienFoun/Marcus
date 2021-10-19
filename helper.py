3b00e4d012b214934b09591aa7ff4b777a0fc571
Матвей Сугоняев <alienfoun@gmail.com>
2021-10-19 21:00:21 +0300

    19.10.2021. Fix

--- helper.py
+++ helper.py
@@ -37,10 +37,18 @@ def appends(cursor, tag_name: str) -> List[str]:
     return tag_list_name, new_tags
 
 
-def words_list_gen(lists: List, tag_list_name: List) -> List[str]:
+def words_list_gen(lists: List[List], tag_list_name: List) -> List[str]:
+    words = []  # Создание промежуточного списка только из слов из базы данных, без их веса
+    for word in tag_list_name:
+        words.append(word[0])  # Добавление слов из базы данных
+
     for element in lists:
-        if element not in tag_list_name:
-            tag_list_name.append(element)
+        if element[0] not in words:  # если слова из входных данных нет в базе
+            tag_list_name.append(element)  # то добавить вместе с весом
+        else:
+            list_index = words.index(element[0])
+            tag_list_name[list_index][1] += element[1]
+
     return tag_list_name
 
 
@@ -61,8 +69,23 @@ def dict_creater(data_list: List) -> dict:
 
 def found_duplication(data_dict: dict, weight_list: list, words_list: list) -> list:
     for element in weight_list:
-        tag_words = data_dict.get(element[0]) # Получаем слова для определенного тэга из базы
+        tag_words = data_dict.get(element[0])  # Получаем слова для определенного тэга из базы
+
+        words = []
+        for el in tag_words:
+            words.append(el[0])
+
         for word in words_list:  # Цикл для проверки наличия входных данных в значениях из базы
-            if word in tag_words:  # Проверяем наличие
-                element[1] += 1  # Прибавляем 1 к счетчику, который в списке тэгов
+            if word in words:  # Проверяем наличие
+                list_index = words.index(word)
+                element[1] += tag_words[list_index][1]
+
     return weight_list
+
+
+def weight_input_calibrator(text: list) -> list:
+    if text != [['']]:
+        for i in range(len(text)):
+            text[i] = [text[i], (len(text[i]) // 5) + 1]
+        return text
+    return []
--- study.py
+++ study.py
@@ -1,5 +1,5 @@
 import json
-from helper import appends, words_list_gen, sanitizer, cutter
+from helper import appends, words_list_gen, sanitizer, cutter, weight_input_calibrator
 from sql import cursor, sql_select_tags, sql_close, sql_insert
 from typing import Dict, List
 
@@ -22,13 +22,13 @@ def update_problems_dict(text: str, tags: List[str]) -> None:
         new_tags += new_value[1]
 
     cutted_words_list: List[str] = cutter(splited_problem_text)
+    calebrated_words_list = weight_input_calibrator(cutted_words_list)
 
     for tag in tags:
         dict_value = problems_dict.get(tag)
-        words_list = json.dumps(cutted_words_list if dict_value == ['']
-                                else words_list_gen(cutted_words_list, dict_value))
+        words_list = json.dumps(calebrated_words_list if dict_value == ['']
+                                else words_list_gen(calebrated_words_list, dict_value))
         sql_select_tags(tag, words_list)
-
         if new_tags:
             for new_tag in new_tags:
                 sql_insert(cursor, new_tag, words_list)
@@ -37,6 +37,6 @@ def update_problems_dict(text: str, tags: List[str]) -> None:
 
 
 MOCK_PROBLEM_TEXT: str = 'Я совершил ошибку и всегда буду их совершать!!!!!!!'.lower()
-MOCK_PROBLEM_TAGS: list = ['due']
+MOCK_PROBLEM_TAGS: list = ['max']
 
 update_problems_dict(MOCK_PROBLEM_TEXT, MOCK_PROBLEM_TAGS)
