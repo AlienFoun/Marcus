c929109c325ed562b4de9d4a3eb18d1827094a65
Матвей Сугоняев <alienfoun@gmail.com>
2021-10-22 19:38:12 +0300

    Fix

--- reply.py
+++ reply.py
@@ -3,30 +3,30 @@ from sql import cursor, database_loads
 from typing import List, Dict
 
 
-def reply_output(text: str, dict_output: Dict[str, Dict[str, int]]) -> List[str]:
+def reply_output(text: str) -> List[str]:
+    DATABASE_OUTPUT: List[Dict[str, Dict[str, int]]] = database_loads(cursor)
+
     output_size = 3
 
     clear_problem_text: str = sanitizer(text)  # Удаление знаков припенания
     splited_problem_text: list = clear_problem_text.split()
 
-    default_weight_list = {}.fromkeys(dict_output.keys(), 0)  # создаем словарь для определения веса тэга, по умолчанию 0
+    default_weight_list = {}.fromkeys(DATABASE_OUTPUT.keys(), 0)  # создаем словарь для определения веса тэга, по умолчанию 0
 
     cutted_words_list: list = cutter(splited_problem_text)
 
-    reply_list: list = found_duplication(dict_output, default_weight_list, cutted_words_list)  # отправляем значения в функцию для определения веса тэга
+    reply_list: list = found_duplication(DATABASE_OUTPUT, default_weight_list, cutted_words_list)  # отправляем значения в функцию для определения веса тэга
     reply_list.sort(key=lambda x: x[1], reverse=True)
 
-    ans = []
+    output_list = []
     for i in range(output_size):
         try:
             if reply_list[i][1] != 0:
-                ans.append(reply_list[i][0])
+                output_list.append(reply_list[i][0])
         except IndexError:
-            return ans
-    return ans
+            return output_list
+    return output_list
 
 
 MOCK_PROBLEM_TEXT: str = 'Я совершил ошибку и всегда буду их совершать!!!!!!!'.lower()
-DATABASE_OUTPUT: List[Dict[str, Dict[str, int]]] = database_loads(cursor)
 
-print(reply_output(MOCK_PROBLEM_TEXT, DATABASE_OUTPUT))
--- server.py
+++ server.py
@@ -1,37 +1,28 @@
-import socket
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from reply import reply_output
+import json
+
+hostname = 'localhost'
+port = 4000
 
 
-def start_server():
-    try:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(('127.0.0.1', 4000))
-        server.listen(100)
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
-        while True:
-            client_socket, address = server.accept()
-            data = client_socket.recv(1024).decode('utf-8')
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        input_data = self.rfile.read(content_length)
 
-            content = load_page_from_request(data)
-            client_socket.send(content)
-            client_socket.shutdown(socket.SHUT_WR)
+        self.send_response(200)
+        self.send_header('Content-type', 'text')
+        self.end_headers()
 
-    except KeyboardInterrupt:
-        server.close()
+        input_text = json.loads(input_data)
 
+        output_list = reply_output(input_text)
+        output_data = json.dumps(output_list)
 
-def load_page_from_request(request_data):
-    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset = utf-8\r\n\r\n'
-    HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset = utf-8\r\n\r\n'
-    path = request_data.split(' ')[1]
-
-    try:
-        with open('views' + path, 'rb') as file:
-            response = file.read()
-        return HDRS.encode('utf-8') + response
-
-    except FileNotFoundError:
-        return (HDRS_404 + 'Error 404. Page not found').encode('utf-8')
+        self.wfile.write(output_data.encode('utf-8'))
 
 
-if __name__ == '__main__':
-    start_server()
+httpd = HTTPServer((hostname, port), SimpleHTTPRequestHandler)
+httpd.serve_forever()
